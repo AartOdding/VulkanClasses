d948b4bfd38381d9e75e1d1e382cd2293e6a4165
Aart <aart_odding@hotmail.com>
2020-12-13 23:02:46 +0100

    Added window creation code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,67 @@
+
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+
+
 
 int main()
 {
-    std::cout << "hello world";
-};
+    glfwSetErrorCallback([](int errorCode, const char* errorDescription) {
+        std::cerr << "GLFW error " << errorCode << ": " << errorDescription << std::endl;
+    });
+
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW." << std::endl;
+        std::exit(1);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
+    glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, true);
+    glfwWindowHint(GLFW_DECORATED, true);
+    glfwWindowHint(GLFW_SCALE_TO_MONITOR, true);
+
+    auto window = glfwCreateWindow(720, 480, "Project name", nullptr, nullptr);
+
+    if (!window)
+    {
+        std::cerr << "Failed to create window." << std::endl;
+        glfwTerminate();
+        std::exit(1);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+        glViewport(0, 0, width, height);
+    });
+
+    if (!gladLoadGL())
+    {
+        std::cerr << "Failed to load OpenGL." << std::endl;
+        glfwTerminate();
+        std::exit(1);
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+
+        glClearColor(0.1, 0, 0.1, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
